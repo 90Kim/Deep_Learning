2f4e0e3cb4f4ce68c3a33c8e859a46fda0b9857c
90Kim <kimgy9090@gmail.com>
2017-10-12 16:28:55 +0900

    finish ch03

--- ch03.py
+++ ch03.py
@@ -226,6 +226,121 @@ y = softmax(a)
 print(y)
 np.sum(y)
 
+##### 3.6 손글씨 숫자 인식 (실전 예제)
+
+import sys, os
+import numpy as np
+os.getcwd()
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+print(img)
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
+
+### 3.6.2 신경망의 추론 처리
+
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+import pickle
+from dataset.mnist import load_mnist
+
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+x, t = get_data()
+network = init_network()
+print(network)
+type(network)
+set(network)
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        accuracy_cnt += 1
+        
+print("Accuracy = " + str(float(accuracy_cnt) / len(x)))
+
+### 3.6.3 배치 처리
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+    
+print("Accuracy = " + str(float(accuracy_cnt) / len(x)))
+
+list(range(0, 10))
+list(range(0, 10, 2))
+
+x = np.array([[0.1, 0.8, 0.1], [0.3, 0.1, 0.6], [0.2, 0.5, 0.3], [0.8, 0.1, 0.1]])
+y = np.argmax(x, axis = 1)
+print(y)
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+mean_squared_error(np.array(y), np.array(t))
+mean_squared_error(y, t)
+
+(np.array(y)).shape
 
 #######################################################
 ###################### finish #########################

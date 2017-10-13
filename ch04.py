bea99a495c4e1f9a3877e42cbe769b5baadca26b
90Kim <kimgy9090@gmail.com>
2017-10-13 16:53:48 +0900

    add ch04.py

---
+++ ch04.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 16:55:23 2017
+
+@author: GooYoung
+"""
+
+#######################################################
+############# chapter 04 신경망 학습 ##################
+#######################################################
+
+import numpy as np
+
+##### 4.2 손실함수 
+
+### 4.2.1 평균 제곱 오차 
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+# 왜 평균 제곱 오차인데 나누기 2야 ? 나누기 n 이어야 되는데 ?
+    
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+mean_squared_error(np.array(y), np.array(t))
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+mean_squared_error(np.array(y), np.array(t))
+
+### 4.2.2 교차 엔트로피 오차
+    
+def cross_entropy_error(y,t):
+    delta = 1e-7
+    return -np.sum(t*np.log(y+delta))
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+cross_entropy_error(np.array(y), np.array(t))
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+cross_entropy_error(np.array(y), np.array(t))
+
+### 4.2.3 미니 배치 학습 
+
+import sys, os
+os.getcwd()
+os.pardir
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+print(x_train.shape)
+print(t_train.shape)
+
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+
+### 4.2.4 (배치용) 교차 엔트로피 오차
+
+def cross_entropy_error(y,t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    return -np.sum(t*np.log(y)) / batch_size
+
+##### 4.3 수치 미분
+    
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+def function_1(x):
+    return 0.01*x**2 + 0.1*x
+
+
+import numpy as np
+import matplotlib.pylab as plt
+
+x = np.arange(0.0, 20.0, 0.1)
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x,y) + (plt.xlabel("x") + plt.ylabel("f(x)"))
+
+numerical_diff(function_1, 5)
+numerical_diff(function_1, 10)
+
+##### 4.4 기울기
+
+def function_2(x):
+    return np.sum(x**2)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+        
+    return grad
+
+numerical_gradient(function_2, np.array([3.0, 4.0]))
+numerical_gradient(function_2, np.array([0.0, 2.0]))
+numerical_gradient(function_2, np.array([3.0, 0.0]))
+        
+
+
+
+#######################################################
+###################### finish##########################
+#######################################################

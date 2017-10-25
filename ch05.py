2fbdf0729feb0cf8a2472a4175053b92a6ffc456
90Kim <kimgy9090@gmail.com>
2017-10-25 17:05:17 +0900

    add & finish chapter 05

---
+++ ch05.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 16:55:23 2017
+
+@author: GooYoung
+"""
+
+#######################################################
+############## chapter 05 오차역전파법 #################
+#######################################################
+
+import numpy as np
+
+##### 5.4 단순한 계층 구현하기
+
+### 5.4.1 곱셈 계층
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+        
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        
+        return dx, dy
+    
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+print(price)
+
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(dapple, dapple_num, dtax)
+
+### 5.4.2 덧셈 계층
+
+class AddLayer():
+    def __init__(self):
+        pass
+    
+    def forward(self, x, y):
+        out = x + y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+    
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+# 계층들 생성
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+print(price)
+
+# 역전파
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+
+print(dapple_num, dapple, dorange, dorange_num, dtax)
+
+##### 5.5 활성화 함수 계층 구현하기
+
+### 5.5.1 ReLU 계층
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+        
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        
+        return out
+    
+    def backward(self, dout):
+        dout[self.maks] = 0
+        dx = dout
+        
+        return dx
+    
+x = np.array([[1.0, -0.5], [-2.0, 3.0]])
+print(x)
+mask = (x<=0)
+print(mask)
+
+### 5.5.2 Sigmoid 계층
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+        
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+    
+##### 5.6 Affine / Softmax 계층
+
+X = np.random.rand(2)
+W = np.random.rand(2,3)
+B = np.random.rand(3)
+X
+W
+B
+
+class Affine:
+    def __init__(self, w, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+        
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.w) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+    
+from common.functions import softmax, cross_entropy_error
+        
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+        
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
+
+### 5.7.2 오차역전파법을 적용한 신경망 구현하기
+
+import numpy as np
+from common.layers import *
+from common.gradient import numerical_gradient
+from collections import OrderedDict
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+        
+        # 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        
+        self.lastLayer = SoftmaxWithLoss()
+        
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        
+        return x
+    
+    # x : 입력 데이터 / t : 정답 레이블
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis = 1)
+        if t.ndim != 1 : t = np.argmax(t, axis = 1)
+
+        accuracy = np.sum(y==t) / float(x.shpae[0])
+        return accuracy
+    
+    # x : 입력 데이터 / t : 정답 레이블
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+            
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        
+        return grads
+    
+    def gradient(self, x, t):
+        # 순전파
+        self.loss(x, t)
+        
+        # 역전파
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        # 결과저장
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+        
+        return grads
+    
+from dataset.mnist import load_mnist
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+    print(key + ":" + str(diff))
+    
+    
+### 5.7.4 오차역전파법을 사용한 학습 구현
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+    
+    # 기울기 계산
+    #grad = network.numerical_gradient(x_batch, t_batch) # 수치 미분 방식
+    grad = network.gradient(x_batch, t_batch) # 오차역전파법 방식(훨씬 빠르다)
+    
+    # 갱신
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+    
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(train_acc, test_acc)
+    
+
+#######################################################
+###################### finish##########################
+#######################################################

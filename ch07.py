b9ed5714c0bcaccb0ea00e73e90d0e87232b96ec
90Kim <kimgy9090@gmail.com>
2017-11-28 14:22:12 +0900

    finish chapter 7

--- ch07.py
+++ ch07.py
@@ -17,6 +17,7 @@ import numpy as np
 
 x = np.random.rand(10, 1, 28, 28)
 x[0].shape
+x[1].shape
 x[0, 0]
 
 ### 7.4.3 합성곱 계층 구현하기
@@ -26,10 +27,12 @@ sys.path.append(os.pardir)
 from common.util import im2col
 
 x1 = np.random.rand(1,3,7,7)
-col1 = im2col(x1, 5, 5, stride=1, pad=0)
+col1 = im2col(x1, 4, 4, stride=1, pad=2)
+print(col1.shape) # (OH**2, filter_h * filter_w * num of channel) 
 
 x2 = np.random.rand(10,3,7,7)
-col2 = im2col(x2, 5, 5, stride=1, pad=0)
+col2 = im2col(x2, 4, 4, stride=1, pad=2)
+print(col2.shape) # (OH**2 * num of data, filter_h * filter_w * num of channel)
 
 class Convolution:
     def __init__(self, W, b, stride=1, pad=0):
@@ -52,7 +55,27 @@ class Convolution:
         
         return out
     
-
+### 7.4.4 풀링 계층 구현하기
+        
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+        
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+        
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+        
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+        
+        return out
 
 
 #######################################################
